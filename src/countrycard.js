93072dc6c4c686356f624ca6c25bda0cf90a3bc8
unknown <cruzechevrolet00@gmail.com>
2024-01-05 12:12:22 +0530

    new

--- src/countrycard.js
+++ src/countrycard.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./countrycard.css";
 
 const CountryCard = ({ country }) => {
   return (
-    <div className="country-card">
+    <div className="country-card" data-testid="country-container">
       <img src={country.flags.png} alt={`${country.name.common} flag`} />
       <p>{country.name.common}</p>
     </div>
